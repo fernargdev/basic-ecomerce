656112ac39158a48984a40e8c7d03add10c89906
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-07-20 03:39:56 -0400

    bill

--- frontend/js/bill.js
+++ frontend/js/bill.js
@@ -1,8 +1,10 @@
 // URL del backend
-const USERS_URL = 'http://localhost:5000/api/users'
-const PROVINCES_URL = 'http://localhost:5000/api/provinces' // Asegúrate de ajustar esta URL
+const STORE_API = 'http://localhost:5000/api/store'
+// TODO:
+// const PAYMENT_API = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 // Elementos del formulario
+const paymentInput = document.querySelector('#payment')
 const emailInput = document.querySelector('#email')
 const nameInput = document.querySelector('#name')
 const addressInput = document.querySelector('#address')
@@ -12,28 +14,74 @@ const provinceSelect = document.querySelector('#province')
 const additionalPhoneInput = document.querySelector('#additional-phone')
 const additionalNameInput = document.querySelector('#additional-name')
 
-// Rellena el formulario con la información del usuario
-const userInfo = JSON.parse(localStorage.getItem('userInfo'))
-emailInput.value = userInfo.email
+// nombre de la tienda de la URL
+// const storeName = window.location.search.split('=')[1]
+const storeName = 'store-1'
 
-// Obtiene las provincias del backend y las añade al select
-axios
-  .get(PROVINCES_URL)
-  .then((response) => {
-    const provinces = response.data
-    for (let province of provinces) {
-      const option = document.createElement('option')
-      option.value = province.id
-      option.textContent = province.name
-      provinceSelect.appendChild(option)
-    }
-  })
-  .catch((error) => console.error(error))
+const getStoreProvince = async () => {
+  try {
+    const res = await axios.get(`${STORE_API}/${storeName}`)
+    const province = res.data.province
+    return province
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-// Añade un event listener al formulario para manejar el envío
+function calculateTotal() {
+  const products = JSON.parse(localStorage.getItem('products')) || []
+  let total = 0
+
+  for (let product of products) {
+    total += parseFloat(product.price.replace(/[^\d]/g, '')) * product.quantity
+  }
+
+  return total.toFixed(2)
+}
+
+const fillFields = async () => {
+  // payment
+  const total = calculateTotal()
+  console.log(total)
+  paymentInput.value = total
+
+  // email
+  const userInfoEmail = JSON.parse(localStorage.getItem('userInfo')).email
+  console.log(userInfoEmail)
+  emailInput.value = userInfoEmail
+
+  // province
+  const province = await getStoreProvince()
+  console.log(province)
+  provinceSelect.value = province
+}
+
+async function handlePayment(e) {
+  e.preventDefault()
+
+  const paymentData = {
+    email: emailInput.value,
+    name: nameInput.value,
+    address: addressInput.value,
+    phone: phoneInput.value,
+    deliveryAddress: deliveryAddressInput.value,
+    province: provinceSelect.value,
+    additionalPhone: additionalPhoneInput.value,
+    additionalName: additionalNameInput.value,
+  }
+
+  try {
+    // await axios.post(PAYMENT_API, paymentData)
+    console.log(paymentData)
+    alert('Pago realizado con éxito')
+    // redirigir al usuario a otra página
+  } catch (error) {
+    console.error(error)
+    alert('Error al realizar el pago')
+  }
+}
+
+document.addEventListener('DOMContentLoaded', fillFields)
 document
   .querySelector('#billing-form')
-  .addEventListener('submit', function (e) {
-    e.preventDefault()
-    // Aquí puedes manejar el envío del formulario
-  })
+  .addEventListener('submit', handlePayment)
